5e9a591005cd36c0939cbb73ed277892c6dd8566
Alireza Alizade <alizade.t488@gmail.com>
2023-03-27 17:52:41 +0300

    first commit

---
+++ lib/widget_tree.dart
@@ -0,0 +1,79 @@
+import 'package:curved_navigation_bar/curved_navigation_bar.dart';
+import 'package:flutter/material.dart';
+import 'package:portfolio_admin_panel/app_bar/app_bar_widget.dart';
+import 'package:portfolio_admin_panel/constants.dart';
+import 'package:portfolio_admin_panel/drawer/drawer_page.dart';
+import 'package:portfolio_admin_panel/panel_center/panel_center_page.dart';
+import 'package:portfolio_admin_panel/panel_left/panel_left_page.dart';
+import 'package:portfolio_admin_panel/panel_right/panel_right_page.dart';
+import 'package:portfolio_admin_panel/responsive_layout.dart';
+
+class WidgetTree extends StatefulWidget {
+  const WidgetTree({Key? key}) : super(key: key);
+
+  @override
+  State<WidgetTree> createState() => _WidgetTreeState();
+}
+
+class _WidgetTreeState extends State<WidgetTree> {
+  int currentIndex = 1;
+
+  final List<Widget> _icons = [
+    const Icon(Icons.add, size: 30),
+    const Icon(Icons.list, size: 30),
+    const Icon(Icons.compare_arrows, size: 30),
+  ];
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: PreferredSize(
+          preferredSize: const Size(double.infinity, 100),
+          child: (ResponsiveLayout.isTinyLimit(context) ||
+                  ResponsiveLayout.isTinyHeightLimit(context)
+              ? Container()
+              : AppBarWidget())),
+      body: ResponsiveLayout(
+        tiny: Container(),
+        phone: currentIndex == 0
+            ? PanelLeftPage()
+            : currentIndex == 1
+            ? PanelCenterPage()
+            : PanelRightPage(),
+        tablet: Row(
+          children: [
+            Expanded(child: PanelLeftPage()),
+            Expanded(child: PanelCenterPage()),
+          ],
+        ),
+        largeTablet: Row(
+          children: [
+            Expanded(child: PanelLeftPage()),
+            Expanded(child: PanelCenterPage()),
+            Expanded(child: PanelRightPage()),
+          ],
+        ),
+        computer: Row(
+          children: [
+            Expanded(child: DrawerPage()),
+            Expanded(child: PanelLeftPage()),
+            Expanded(child: PanelCenterPage()),
+            Expanded(child: PanelRightPage()),
+          ],
+        ),
+      ),
+      drawer: DrawerPage(),
+      bottomNavigationBar: ResponsiveLayout.isPhone(context)
+          ? CurvedNavigationBar(
+        index: currentIndex,
+        backgroundColor: Constants.purpleDark,
+        items: _icons,
+        onTap: (index) {
+          setState(() {
+            currentIndex = index;
+          });
+        },
+      )
+          : const SizedBox(),
+    );
+  }
+}
